91d821026e374d2583df35a870cd4cd5e6312194
soliman Abd <marksol36@gmail.com>
2021-05-08 18:09:35 -0400

    Add Helmet to my ems application

--- ems/app.js
+++ ems/app.js
@@ -13,6 +13,8 @@ var logger = require('morgan');
 var Employee = require("./models/employee");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var helmet = require("helmet");
+
 
 // connect to mongoose database
 var mongoDB = 'mongodb+srv://Soliman:Abdelmalak_@cluster0.rpzcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
@@ -27,6 +29,7 @@ db.once('open', function() {
 });
 var app = express();
 app.use(logger("short"));//use morgan for logging
+app.use(helmet.xssFilter());
 app.use(bodyParser.urlencoded ({  // Use Body Parser to parse the incoming request
   extended: true
 }));
@@ -58,7 +61,7 @@ app.get("/about", function(request, response) {
 app.get("/contact", function(request, response) {
   response.render("contact", {
       message: "contact page"
-  })
+  });
 });
 // Create a function that accept and response to the products page request.
 app.get("/products", function(request, response) {
@@ -66,7 +69,15 @@ app.get("/products", function(request, response) {
      message: "products page"
  });
 });
-// create server
+
+ // http calls
+app.get("/", function(request, response) {
+  response.render("index", {
+      message: "XSS Prevention Example"
+  });
+
+});
+// create/start Node server
 http.createServer(app).listen(8080, function() {
     console.log("Application started on port 8080!");
 });
