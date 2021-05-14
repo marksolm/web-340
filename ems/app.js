9deca4e04fdb0599f6e143ae926ee8f391621edc
soliman Abd <marksol36@gmail.com>
2021-05-14 13:32:07 -0400

    final update by Adiing a scripts section and npm start call,creat a Procfile and  .gitignore files

--- ems/app.js
+++ ems/app.js
@@ -146,9 +146,7 @@ Employee.find({ lastName: queryName }, function (error, employees) {
   }
 });
 });
-
-
 // create/start Node server
-http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080!");
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("Application started on port " + app.get("port"));
 });
